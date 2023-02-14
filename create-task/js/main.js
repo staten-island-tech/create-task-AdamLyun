4a0002b1be50503e7fd26e51907960ab606507d5
AdamLyun <113201169+AdamLyun@users.noreply.github.com>
2023-02-13 23:09:10 -0500

    fbgnhrt

--- create-task/js/main.js
+++ create-task/js/main.js
@@ -1,72 +1,45 @@
 import { DOMSelectors } from "./dom";
 import { items } from "./array";
 
-
-function random_item(items)
-{
-  
-return items[Math.floor(Math.random()*items.length)];
-     
+function random_item(items) {
+  return items[Math.floor(Math.random() * items.length)];
 }
 
 
-console.log(random_item(items));
+let a = random_item(items);
+let b= random_item(items);
+let c= a+b
 
-
-
-DOMSelectors.button.addEventListener("click", function () {
-factual()
-  something()
-  somethingelse()
-console.log(x)
-});
-
-
-let x= random_item(items)+random_item(items)
-
-function factual(){
-  while (x>=19) {
-    if (x>=19) {
-      console.log(`You win`)
-      
-    }  else if (x>19){
-      DOMSelectors.box.insertAdjacentHTML(
-        "beforeend",
-        `
-        <button class="amongus">Remove</button>
-  
-    `
-      );
-      
-    } else{
-      DOMSelectors.box.insertAdjacentHTML(
-        "beforeend",
-        `
+function factual() {
+console.log(a)
+console.log(b)
+console.log(c)
+  if (c >= 21) {
+    console.log(`You win`);
+  } else {
+    DOMSelectors.box.insertAdjacentHTML(
+      "beforeend",
+      `
         <button class="frrr">Remove</button>
   
-    `)
-    }
-    
-
-
+    `
+    );
   }
+
 }
-function something(){
-  let fax= document.querySelectorAll(".amongus")
-  fax.forEach((easy)=>
-  {
-    easy.addEventListener("click", function(){
-      let q= x+ random_item(items)
-      
-    })
-  })
+function something() {
+  let fax = document.querySelectorAll(".frrr");
+  fax.forEach((easy) => {
+    easy.addEventListener("click", function () {
+     let d = random_item(items);
+     let e= c+d
+      console.log(e);
+    });
+  });
 }
-function somethingelse(){
-  let fax= document.querySelectorAll(".amongus")
-  fax.forEach((easy)=>
-  {
-    easy.addEventListener("click", function(){
-      console.log("bye")
-    })
-  })
-}
+
+DOMSelectors.button.addEventListener("click", function () {
+  factual();
+  something();
+  
+});
